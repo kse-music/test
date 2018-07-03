62ac56f2288a96823772857ad54b3435bdfd0558
DingHao <dh.hiekn@gmail.com>
2018-07-04 00:07:46 +0800

    spring boot demo

---
+++ src/main/java/com/hiekn/test/rest/TestRestApi.java
@@ -0,0 +1,20 @@
+package com.hiekn.test.rest;
+
+import com.hiekn.boot.autoconfigure.base.model.result.RestResp;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("test")
+@Api("测试")
+public class TestRestApi {
+
+    @GET
+    @Path("list")
+    @ApiOperation("列表")
+    public RestResp list(){
+        return new RestResp("Hello Spring Boot");
+    }
+}
