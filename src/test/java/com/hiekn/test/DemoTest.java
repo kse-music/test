46b531a9234fbbe348dee5f602b0d8e4b777bdc6
kse-music <dh.hiekn@gmail.com>
2018-07-24 13:11:38 +0800

    none

---
+++ src/test/java/com/hiekn/test/DemoTest.java
@@ -0,0 +1,9 @@
+package com.hiekn.test;
+
+import com.hiekn.boot.autoconfigure.base.util.MybatisGenUtil;
+
+public class DemoTest {
+    public static void main(String[] args) {
+        MybatisGenUtil.genMapperAndXml();
+    }
+}
