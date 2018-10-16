db5e40d750e08ebfe6288dc6235ed8287f90c749
kse-music <dh.hiekn@gmail.com>
2018-10-16 15:56:17 +0800

    3.0.1

--- src/main/java/com/hiekn/test/rest/TestRestApi.java
+++ src/main/java/com/hiekn/test/rest/TestRestApi.java
@@ -3,15 +3,19 @@ package com.hiekn.test.rest;
 import com.hiekn.boot.autoconfigure.base.model.result.RestResp;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
 
 @Path("test")
 @Api("测试")
 @Controller
 public class TestRestApi {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @GET
     @Path("list")
@@ -19,4 +23,14 @@ public class TestRestApi {
     public RestResp list(){
         return new RestResp("Hello Spring Boot");
     }
+
+    @POST
+    @Path("post")
+    @ApiOperation("post")
+    public RestResp post(@QueryParam("userId") String userId,
+                         @FormParam("bean")String bean,
+                         @FormParam("bean2")String bean2){
+        return new RestResp(request.getParameterMap());
+    }
+
 }
