86c8c35eaaaa455c5718a961160eadfdb991f98c
kse-music <dh.hiekn@gmail.com>
2018-11-01 10:16:45 +0800

    3.0.3

--- src/main/java/com/hiekn/test/rest/TestRestApi.java
+++ src/main/java/com/hiekn/test/rest/TestRestApi.java
@@ -1,5 +1,6 @@
 package com.hiekn.test.rest;
 
+import com.google.common.collect.Lists;
 import com.hiekn.boot.autoconfigure.base.model.result.RestResp;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,8 +9,11 @@ import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("test")
+@Produces(MediaType.APPLICATION_JSON)
 @Api("测试")
 @Controller
 public class TestRestApi {
@@ -31,6 +35,13 @@ public class TestRestApi {
                          @FormParam("bean")String bean,
                          @FormParam("bean2")String bean2){
         return new RestResp(request.getParameterMap());
+    }
+
+    @POST
+    @Path("extract")
+    @ApiOperation("extract")
+    public List<WordBean> extract(@FormParam("text")String text){
+        return Lists.newArrayList(new WordBean("test","test"),new WordBean("test2","test2"),new WordBean("test","test"));
     }
 
 }
