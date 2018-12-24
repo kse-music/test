683892efd691677712ba2e903036703af8de7777
kse-music <dh.hiekn@gmail.com>
2018-12-24 16:23:54 +0800

    use mcn maven plugin

--- src/test/java/com/hiekn/test/DemoTest.java
+++ src/test/java/com/hiekn/test/DemoTest.java
@@ -1,24 +1,15 @@
 package com.hiekn.test;
 
 import com.hiekn.boot.autoconfigure.base.util.McnUtils;
-import com.hiekn.boot.autoconfigure.base.util.MybatisGenUtil;
 import org.junit.Test;
-
-import java.util.Properties;
 
 public class DemoTest extends TestBase{
 
 
     @Test
     public void testDemo(){
-        Properties properties = McnUtils.loadProperties("generator.properties");
-        logger.info("{}",properties);
-
-    }
-
-    @Test
-    public void genMapperAndXml() {
-        MybatisGenUtil.genMapperAndXml();
+        logger.info("{}",McnUtils.randomUUID());
+        logger.info("{}",McnUtils.simpleUUID());
     }
 
 }
