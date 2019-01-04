ce25f356783409b3f769c31c7398f242b701f657
kse-music <dh.hiekn@gmail.com>
2019-01-04 18:31:04 +0800

    test mcn util

--- src/test/java/com/hiekn/test/McnUtilTest.java
+++ src/test/java/com/hiekn/test/McnUtilTest.java
@@ -15,9 +15,11 @@ public class McnUtilTest extends TestBase{
 
         //load prop
         //自动从指定的key中识别，环境变量优先级高于java属性
-
         logger.info("{}",McnUtils.loadProperties("a.properties","k"));
+
+        McnUtils.copyFile("G:/TeamViewer_13.2.14327.0.exe", "d:/TeamViewer_13.2.14327.0.exe");
 
     }
 
 }
+
