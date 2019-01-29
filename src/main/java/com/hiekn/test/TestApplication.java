1362b56b51c7271d77a7460f331ee4021eae0d1b
kse-music <dh.hiekn@gmail.com>
2019-01-29 17:33:51 +0800

    add sql

--- src/main/java/com/hiekn/test/TestApplication.java
+++ src/main/java/com/hiekn/test/TestApplication.java
@@ -1,20 +1,14 @@
 package com.hiekn.test;
 
-import com.hiekn.test.dao.NewsMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(TestApplication.class, args);
-        NewsMapper bean = run.getBean(NewsMapper.class);
-        System.out.println(bean.selectByCondition(null));
+        SpringApplication.run(TestApplication.class, args);
     }
-
-
 
 }
