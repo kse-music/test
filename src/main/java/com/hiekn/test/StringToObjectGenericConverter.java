e03ca19d36dd872d5c567d57595789e02d7aad13
DingHao <dh.hiekn@gmail.com>
2019-07-05 19:23:00 +0800

    add lombok

--- src/main/java/com/hiekn/test/StringToObjectGenericConverter.java
+++ src/main/java/com/hiekn/test/StringToObjectGenericConverter.java
@@ -6,16 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.GenericConverter;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 @Component
-public class StringToObjectGenericConverter implements GenericConverter, WebMvcConfigurer {
+public class StringToObjectGenericConverter implements GenericConverter{
 
     @Autowired
     private Gson gson;
@@ -31,11 +29,6 @@ public class StringToObjectGenericConverter implements GenericConverter, WebMvcC
     public Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
         ResolvableType resolvableType = targetType.getResolvableType();
         return gson.fromJson(source.toString(),resolvableType.hasGenerics()?resolvableType.getType():resolvableType.resolve());
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(this);
     }
 
 }
