d4478826cd1787291f34acc20772e63dac09aa17
DingHao <dh.hiekn@gmail.com>
2021-05-11 10:19:41 +0800

    设置数据库编码

--- src/main/resources/db/schema-mysql.sql
+++ src/main/resources/db/schema-mysql.sql
@@ -1,3 +1,5 @@
+alter database meta_boot character set utf8mb4 collate utf8mb4_general_ci;
+
 CREATE TABLE `t_news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(64) NOT NULL,
@@ -6,4 +8,4 @@ CREATE TABLE `t_news` (
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `tc` (`title`,`cid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
